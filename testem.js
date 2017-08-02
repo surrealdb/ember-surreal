4f0d3bd7d3fafc70fd79486a50552c25fee98cb6
Tobie Morgan Hitchcock <tobie@abcum.com>
2017-08-02 17:10:39 +0100

    Use Chrome for testing in CI environment

--- testem.js
+++ testem.js
@@ -1,12 +1,20 @@
 /* eslint-env node */
 module.exports = {
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "launch_in_ci": [
-    "PhantomJS"
+  test_page: 'tests/index.html?hidepassed',
+  disable_watching: true,
+  launch_in_ci: [
+    'Chrome'
   ],
-  "launch_in_dev": [
-    "PhantomJS",
-    "Chrome"
-  ]
+  launch_in_dev: [
+    'Chrome'
+  ],
+  browser_args: {
+    'Chrome': [
+      '--headless',
+      '--no-sandbox',
+      '--disable-gpu',
+      '--remote-debugging-port=9222',
+      '--remote-debugging-address=0.0.0.0'
+    ]
+  }
 };
