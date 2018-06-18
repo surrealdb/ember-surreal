11f4ec1dc24a70049ca545f1a5202f04fc650901
Tobie Morgan Hitchcock <tobie@abcum.com>
2018-06-18 07:59:40 +0100

    Add SQL preprocessor type for ES6

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 'use strict';
 
+const Filter = require('broccoli-persistent-filter');
+
+class SQLFilter extends Filter {
+
+  constructor(inputTree, options) {
+    super(inputTree, options);
+    this.extensions = ['sql'];
+    this.targetExtension = 'js';
+  }
+
+  getDestFilePath(path) {
+    return null;
+  }
+
+  processString(source) {
+    return "export default " + JSON.stringify(source) + ";";
+  }
+
+}
+
 module.exports = {
-  name: 'ember-surreal'
+  name: 'ember-surreal',
+  setupPreprocessorRegistry(type, registry) {
+    if (type === 'parent') {
+        registry.add('js', {
+          name: 'ember-surreal',
+          ext: 'sql',
+          toTree(tree) {
+            return new SQLFilter(tree);
+          }
+        });
+    }
+  }
 };
