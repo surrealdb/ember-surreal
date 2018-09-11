125a6bfa8d70e51c92e30ad82c8ee3a3b49aefbf
Tobie Morgan Hitchcock <tobie@abcum.com>
2018-09-11 17:30:19 +0100

    Extract any embedded relationships from DB results

--- addon/serializers/surreal.js
+++ addon/serializers/surreal.js
@@ -1,4 +1,5 @@
-import DS from "ember-data";
+import DS from 'ember-data';
+import { get } from '@ember/object';
 
 export default DS.SurrealSerializer = DS.JSONSerializer.extend({
 
@@ -16,10 +17,10 @@ export default DS.SurrealSerializer = DS.JSONSerializer.extend({
 
 		snapshot.eachRelationship( (key, attr) => {
 			if (!attr.options.readOnly) {
-				if (attr.kind == 'hasMany') {
+				if (attr.kind === 'hasMany') {
 					this.serializeHasMany(snapshot, json, attr);
 				}
-				if (attr.kind == 'belongsTo') {
+				if (attr.kind === 'belongsTo') {
 					this.serializeBelongsTo(snapshot, json, attr);
 				}
 			}
@@ -40,18 +41,26 @@ export default DS.SurrealSerializer = DS.JSONSerializer.extend({
 	},
 
 	extractMeta(store, type, payload) {
+
 		if (payload && payload.meta) {
 			// Don't delete meta from payload!
 			return payload.meta;
 		}
+
 		return null;
+
 	},
 
 	normalize(type, hash) {
+
 		if (hash && hash.meta) {
 			hash.type = hash.meta.tb;
 		}
-		return this._super(type, hash);
+
+		let data = this._super(type, hash);
+
+		return this.extractEmbeddedRecords(this.store, type, data);
+
 	},
 
 	normalizeArrayResponse(store, type, records, id, method) {
@@ -60,6 +69,82 @@ export default DS.SurrealSerializer = DS.JSONSerializer.extend({
 
 	normalizeSingleResponse(store, type, records, id, method) {
 		return this._super(store, type, records[0] || {}, id, method);
+	},
+
+	normalizeEmbeddedRelationship(store, attr, data) {
+
+		let modelName = attr.type;
+
+		if (attr.options.polymorphic) {
+			modelName = data.type;
+		}
+
+		let modelClass = store.modelFor(modelName);
+		let serializer = store.serializerFor(modelName);
+
+		return serializer.normalize(modelClass, data, null);
+
+	},
+
+	extractEmbeddedRecords(store, type, hash) {
+
+		hash.included = hash.included || [];
+
+		type.eachRelationship( (key, attr) => {
+			if (attr.kind === "hasMany") {
+				this.extractEmbeddedHasMany(store, key, hash, attr);
+			}
+			if (attr.kind === "belongsTo") {
+				this.extractEmbeddedBelongsTo(store, key, hash, attr);
+			}
+		});
+
+		return hash;
+
+	},
+
+	extractEmbeddedHasMany(store, key, hash, attr) {
+
+		let items = get(hash, `data.relationships.${key}.data`);
+
+		if (!items) return;
+
+		for (let i = 0; i < items.length; i++) {
+
+			items[i] = this.extractEmbeddedRecord(store, attr, hash, items[i]);
+
+		}
+
+	},
+
+	extractEmbeddedBelongsTo(store, key, hash, attr) {
+
+		let item = get(hash, `data.relationships.${key}.data`);
+
+		if (!item) return;
+
+		item = this.extractEmbeddedRecord(store, attr, hash, item);
+
+	},
+
+	extractEmbeddedRecord(store, attr, hash, item) {
+
+		if ( Object.keys(item).length === 2 ) {
+			if (item.id && item.type) {
+				return item;
+			}
+		}
+
+		let { data, included } = this.normalizeEmbeddedRelationship(store, attr, item);
+
+		hash.included.push(data);
+
+		if (included) {
+			hash.included.push(...included);
+		}
+
+		return { id: data.id, type: data.type };
+
 	},
 
 });
